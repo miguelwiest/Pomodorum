284a7682552c0e1b64b44b12c0e769dc340a36b6
Miguel Wiest <wiestmiguel@gmail.com>
2021-09-24 17:09:19 -0300

    user photo and username added

--- pages/auth/signout.vue
+++ pages/auth/signout.vue
@@ -1,7 +1,25 @@
 <template>
-	<h1 class="login-darkMode flex justify-center items-center">
-		You Already Logged Out
+	<div>
+	<h1
+		class="
+		login-darkMode
+		flex
+		justify-center
+		items-center"
+	>
+		You Already Logged Out,
+		return to login
 	</h1>
+	<a
+		href="/auth/signin"
+		class="login-darkMode
+		flex
+		justify-center
+		items-center"
+	>
+		RETURN
+	</a>
+	</div>
 </template>
 
 <script>
